08bb8aa383888a909fa6839d935c075d053cc77d
GusKSP <gus.paulino@sptech.school>
2026-05-30 17:01:09 -0300

    Adicionando mais ações ao model do Usuario

--- web-data-viz-main/src/models_rpg_reimagine/UsuarioRpgModel.js
+++ web-data-viz-main/src/models_rpg_reimagine/UsuarioRpgModel.js
@@ -16,8 +16,7 @@ function cadastrarRpg(nome, email, senha) {
 function verificarEmail(email) {
     console.log("ACESSEI O USUARIORPG MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarRpg()");
     var instrucaoSelectUser = `            
-    SELECT u.email FROM Usuario u
-    WHERE u.email = (?);`;
+    SELECT u.email FROM Usuario u WHERE u.email = (?);`;
     console.log("Executando a instrução SQL: \n" + instrucaoSelectUser);
     return database.executar(instrucaoSelectUser, [email]);
 }
@@ -26,18 +25,29 @@ function autenticarUsuario(email, senha) {
     console.log("ACESSEI O USUARIORPG MODEL")
 
 
-    var instrucaoSelectUser = `SELECT u.id_usuario, u.email, u.imagem_usuario, u.nome, u.super_usuario FROM Usuario u WHERE u.email= ? AND u.senha = ?`
+    var instrucaoSelectUser = `
+    SELECT u.id_usuario, u.email, u.imagem_usuario, u.nome, u.super_usuario FROM Usuario u WHERE u.email= ? AND u.senha = ?;`
     console.log("Executando a instrução SQL: \n" + instrucaoSelectUser);
     return database.executar(instrucaoSelectUser, [email, senha])
 }
 
 function alterarFoto(imagem, id) {
     console.log("ACESSEI O USUARIORPG MODEL")
-    var instrucaoUpdateUser = `UPDATE usuario SET imagem_usuario = ? WHERE id_usuario = ?`
+    var instrucaoUpdateUser = `UPDATE Usuario SET imagem_usuario = ? WHERE id_usuario = ?`
 
     console.log("Executando a instrução SQL: \n" + instrucaoUpdateUser);
 
     return database.executar(instrucaoUpdateUser, [imagem, id])
+
+}
+
+function alterarNome(nome, id) {
+    console.log("ACESSEI O USUARIORPG MODEL")
+    var instrucaoUpdateUser = `UPDATE Usuario SET nome = ? WHERE id_usuario = ?`
+
+    console.log("Executando a instrução SQL: \n" + instrucaoUpdateUser);
+
+    return database.executar(instrucaoUpdateUser, [nome, id])
 
 }
 
